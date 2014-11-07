9de334fc0d8415c1bfa689ff97090bf38bc1f81a
Norman Mu <thenormanmu@gmail.com>
2014-11-06 21:41:37 -0800

    Cleaned up UI
    Removed unnecessary information, rotated output display.

--- weatherman.py
+++ weatherman.py
@@ -4,20 +4,33 @@ import requests
 print('Finding location from IP address')
 
 def get_temps(city, state):
+	#send request with corrected args
 	r2 = requests.get('http://api.wunderground.com/api/78c2f37e6d924b1b/hourly/q/%s/%s.json' % (state, city))
 	forecast = json.loads(r2.text)
-	for x in range(0, 35):
+	windy = []
+	
+	for x in range(0, 12):
 		civil = forecast['hourly_forecast'][x]['FCTTIME']['civil']
+		wspd = int(forecast['hourly_forecast'][x]['wspd']['english'])
+		if wspd >= 10:
+			windy.append(civil)
+		print('%8s  ' % civil, end = "")
+
+	print('\n')
+	for x in range(0, 12):
 		temp = int(forecast['hourly_forecast'][x]['temp']['english'])
-		bars = round(temp / 5)
-		feelslike = forecast['hourly_forecast'][x]['feelslike']['english']
-		wspd = forecast['hourly_forecast'][x]['wspd']['english']
-		if temp > int(forecast['hourly_forecast'][x+1]['temp']['english']):
-			print('%8s: %3s°F, feels like %3s°F, with %2s mph winds' % (civil, temp, feelslike, wspd), ' ' * bars,'/')
-		elif temp == int(forecast['hourly_forecast'][x]['temp']['english']):
-			print('%8s: %3s°F, feels like %3s°F, with %2s mph winds' % (civil, temp, feelslike, wspd), ' ' * bars, '|')
-		elif temp < int(forecast['hourly_forecast'][x]['temp']['english']):
-			print('%8s: %3s°F, feels like %3s°F, with %2s mph winds' % (civil, temp, feelslike, wspd), ' ' * bars, '\\')
+		print('  %3s°F   ' % temp, end = '')
+		
+	print('\n')
+	for x in range(0, 12):
+		feelslike = int(forecast['hourly_forecast'][x]['feelslike']['english'])
+		print('  %3s°F   ' % feelslike, end = '')
+	
+	print('\n\nRow 1: Time\nRow 2: Actual temperature\nRow 3: Adjusted temperature (for wind chill)')
+	if len(windy) > 0:
+		print('\nIt sure is windy today! Higher than 10 mph winds today at: ', windy)
+	
+	print('\n')	
 	
 #use wundergound geolookup api
 r1 = requests.get('http://api.wunderground.com/api/78c2f37e6d924b1b/geolookup/q/autoip.json')
@@ -26,15 +39,14 @@ state = location['location']['state']
 city = location['location']['city']
 
 #make sure geo is correct
-#print('%s, %s' % (city, state))
-print(city)
-print(state)
+print('%s, %s' % (city, state))
 correct = ''
 
 while (correct != 'y') and (correct != 'n'):
 	correct = input('\nIs this correct? y/n: ')
 if correct == 'y':
 	try:
+		print('\nHere\'s how the next 12 hours look!\n\n')
 		get_temps(city, state)
 	except:
 		print('Oops! Invalid city or state')
@@ -43,7 +55,7 @@ if correct == 'n':
 	try:
 		city = input('Please input city: ')
 		state = input('Please input state initials: ')
+		print('\nHere\'s how the next 12 hours look!\n\n')
 		get_temps(city.replace(' ', '_'), state)
 	except:
-		#r3 = requests.get('http://autocomplete.wunderground.com/aq?query=%s+%s' % (city, state))
 		print('\nOops! Invalid city or state')
